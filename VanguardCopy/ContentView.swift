39123e0b9dad91a19cdc4a830cd75c8584c82ee4
Kirtan Patel <kirtanpatl@gmail.com>
2021-01-29 16:53:15 -0500

    Initial Commit

---
+++ VanguardCopy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VanguardCopy
+//
+//  Created by Kirtan Patel on 1/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VanguardCopy/VanguardCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VanguardCopyApp.swift
+//  VanguardCopy
+//
+//  Created by Kirtan Patel on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct VanguardCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
